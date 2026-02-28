31d594181f27cb3bccbaf0e09141ca9d390faf59
zhangzibo <zzb1230920@163.com>
2026-02-28 12:24:38 +0800

    feat(leetcode): 添加70题-爬楼梯DP解法

---
+++ src/main/java/org/example/dp/ClimbStairs.java
@@ -0,0 +1,15 @@
+package org.example.dp;
+
+// LeetCode 70. 爬楼梯
+public class ClimbStairs {
+    public int climbStairs(int n) {
+        if (n <= 2) return n;
+        int a = 1, b = 2;
+        for (int i = 3; i <= n; i++) {
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
+}
