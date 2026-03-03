2524a690b0680d735135c4c040484dd92423ecd8
zhangzibo <zzb1230920@163.com>
2026-03-03 23:06:31 +0800

    feat(leetcode): 添加11题-盛最多水的容器

---
+++ src/main/java/org/example/TwoPointer/MaxArea.java
@@ -0,0 +1,15 @@
+package org.example.TwoPointer;
+
+// LeetCode 11. 盛最多水的容器
+public class MaxArea {
+    public int maxArea(int[] height) {
+        int left = 0, right = height.length - 1, max = 0;
+        while (left < right) {
+            int area = Math.min(height[left], height[right]) * (right - left);
+            max = Math.max(max, area);
+            if (height[left] < height[right]) left++;
+            else right--;
+        }
+        return max;
+    }
+}
