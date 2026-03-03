69f918a44da5293ddc6e3f1d9a45071cb227e9ac
zhangzibo <zzb1230920@163.com>
2026-03-03 23:07:07 +0800

    feat(leetcode): 添加20题-有效的括号

---
+++ src/main/java/org/example/singleStack/IsValid.java
@@ -0,0 +1,17 @@
+package org.example.singleStack;
+
+import java.util.Stack;
+
+// LeetCode 20. 有效的括号
+public class IsValid {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(') stack.push(')');
+            else if (c == '[') stack.push(']');
+            else if (c == '{') stack.push('}');
+            else if (stack.isEmpty() || stack.pop() != c) return false;
+        }
+        return stack.isEmpty();
+    }
+}
