75091545d4134d844c09286ac00b3caac7bff82b
zhangzibo <zzb1230920@163.com>
2026-03-03 23:06:19 +0800

    feat(leetcode): 添加53题-最大子数组和

---
+++ src/main/java/org/example/dp/MaxSubArray.java
@@ -0,0 +1,13 @@
+package org.example.dp;
+
+// LeetCode 53. 最大子数组和
+public class MaxSubArray {
+    public int maxSubArray(int[] nums) {
+        int max = nums[0], cur = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            cur = Math.max(nums[i], cur + nums[i]);
+            max = Math.max(max, cur);
+        }
+        return max;
+    }
+}
