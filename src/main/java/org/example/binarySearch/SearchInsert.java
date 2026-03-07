b6b88afc2b16f81e10de2a29678daec04f0f36c1
zhangzibo <zzb1230920@163.com>
2026-03-07 10:20:00 +0800

    docs: 优化SearchInsert注释

--- src/main/java/org/example/binarySearch/SearchInsert.java
+++ src/main/java/org/example/binarySearch/SearchInsert.java
@@ -1,6 +1,7 @@
 package org.example.binarySearch;
 
 // LeetCode 35. 搜索插入位置
+// 二分查找返回插入位置
 public class SearchInsert {
     public int searchInsert(int[] nums, int target) {
         int left = 0, right = nums.length - 1;
