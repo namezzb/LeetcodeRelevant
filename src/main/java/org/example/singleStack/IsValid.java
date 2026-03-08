564ab6b85f00365e801e8d4b1731bfb026c7f576
zhangzibo <zzb1230920@163.com>
2026-03-08 14:20:00 +0800

    docs: 优化IsValid注释

--- src/main/java/org/example/singleStack/IsValid.java
+++ src/main/java/org/example/singleStack/IsValid.java
@@ -3,6 +3,7 @@ package org.example.singleStack;
 import java.util.Stack;
 
 // LeetCode 20. 有效的括号
+// 使用栈匹配括号对
 public class IsValid {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
