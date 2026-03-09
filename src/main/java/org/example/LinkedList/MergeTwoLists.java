beba0256323ec89433a52e1b4f915c84fb72177b
zhangzibo <zzb1230920@163.com>
2026-03-09 21:04:32 +0800

    feat(leetcode): 添加21题-合并两个有序链表

---
+++ src/main/java/org/example/LinkedList/MergeTwoLists.java
@@ -0,0 +1,26 @@
+package org.example.LinkedList;
+
+// LeetCode 21. 合并两个有序链表
+public class MergeTwoLists {
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0), cur = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        cur.next = l1 != null ? l1 : l2;
+        return dummy.next;
+    }
+}
