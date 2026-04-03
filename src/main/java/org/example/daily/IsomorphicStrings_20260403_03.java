12ffda9fff159a3490c5f37ea62e52673bd6291b
zhangzibo <zzb1230920@163.com>
2026-04-03 13:30:00 +0800

    solve: IsomorphicStrings

---
+++ src/main/java/org/example/daily/IsomorphicStrings_20260403_03.java
@@ -0,0 +1,11 @@
+package org.example.daily;
+public class IsomorphicStrings_20260403_03 {
+    public boolean isIsomorphic(String s, String t) {
+        int[] ms = new int[256], mt = new int[256];
+        for (int i = 0; i < s.length(); i++) {
+            if (ms[s.charAt(i)] != mt[t.charAt(i)]) return false;
+            ms[s.charAt(i)] = i + 1; mt[t.charAt(i)] = i + 1;
+        }
+        return true;
+    }
+}
