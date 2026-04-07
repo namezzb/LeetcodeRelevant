b6dd7024d4e81cb190bebbf0f62f289d35e5da0f
zhangzibo <zzb1230920@163.com>
2026-04-07 13:30:00 +0800

    solve: MergeTwoLists

---
+++ src/main/java/org/example/daily/MergeTwoLists_20260407_03.java
@@ -0,0 +1,14 @@
+package org.example.daily;
+public class MergeTwoLists_20260407_03 {
+    static class ListNode { int val; ListNode next; ListNode(int v) { val = v; } }
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode d = new ListNode(0), t = d;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) { t.next = l1; l1 = l1.next; }
+            else { t.next = l2; l2 = l2.next; }
+            t = t.next;
+        }
+        t.next = (l1 != null) ? l1 : l2;
+        return d.next;
+    }
+}
