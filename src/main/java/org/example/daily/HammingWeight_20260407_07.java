96f89d7270b2942f4741987090de22ce795cc5f7
zhangzibo <zzb1230920@163.com>
2026-04-07 21:30:00 +0800

    solve: HammingWeight

---
+++ src/main/java/org/example/daily/HammingWeight_20260407_07.java
@@ -0,0 +1,8 @@
+package org.example.daily;
+public class HammingWeight_20260407_07 {
+    public int hammingWeight(int n) {
+        int c = 0;
+        while (n != 0) { n &= (n - 1); c++; }
+        return c;
+    }
+}
