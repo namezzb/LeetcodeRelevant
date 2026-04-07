773348825661cafc77fac2aaf234f1dd691c39e2
zhangzibo <zzb1230920@163.com>
2026-04-07 09:30:00 +0800

    solve: ContainsDuplicate

---
+++ src/main/java/org/example/daily/ContainsDuplicate_20260407_01.java
@@ -0,0 +1,8 @@
+package org.example.daily;
+public class ContainsDuplicate_20260407_01 {
+    public boolean containsDuplicate(int[] nums) {
+        var set = new java.util.HashSet<Integer>();
+        for (int n : nums) if (!set.add(n)) return true;
+        return false;
+    }
+}
