4aa781fc942d4636fb78ebf44f271410b71a0145
zhangzibo <zzb1230920@163.com>
2026-04-10 13:30:00 +0800

    solve: ValidAnagram

---
+++ src/main/java/org/example/daily/ValidAnagram_20260410_03.java
@@ -0,0 +1,10 @@
+package org.example.daily;
+public class ValidAnagram_20260410_03 {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+        int[] cnt = new int[26];
+        for (char c : s.toCharArray()) cnt[c - 'a']++;
+        for (char c : t.toCharArray()) if (--cnt[c - 'a'] < 0) return false;
+        return true;
+    }
+}
