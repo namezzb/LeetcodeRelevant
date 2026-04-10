4da2d78f0221b8d76381fd378c662664e2756b29
zhangzibo <zzb1230920@163.com>
2026-04-10 21:30:00 +0800

    solve: SqrtX

---
+++ src/main/java/org/example/daily/SqrtX_20260410_07.java
@@ -0,0 +1,9 @@
+package org.example.daily;
+public class SqrtX_20260410_07 {
+    public int mySqrt(int x) {
+        if (x == 0) return 0;
+        long r = x;
+        while (r * r > x) r = (r + x / r) / 2;
+        return (int) r;
+    }
+}
