e7edb19e91277e3d6317e1b48c72446917dd15db
zhangzibo <zzb1230920@163.com>
2026-04-11 11:00:00 +0800

    solve: ValidParentheses

---
+++ src/main/java/org/example/daily/ValidParentheses_20260411_02.java
@@ -0,0 +1,13 @@
+package org.example.daily;
+public class ValidParentheses_20260411_02 {
+    public boolean isValid(String s) {
+        var st = new java.util.ArrayDeque<Character>();
+        for (char c : s.toCharArray()) {
+            if (c == '(') st.push(')');
+            else if (c == '{') st.push('}');
+            else if (c == '[') st.push(']');
+            else if (st.isEmpty() || st.pop() != c) return false;
+        }
+        return st.isEmpty();
+    }
+}
