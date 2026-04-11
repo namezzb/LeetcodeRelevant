63dbbb7944b33716a77f2b5153aa472558e00872
zhangzibo <zzb1230920@163.com>
2026-04-11 09:30:00 +0800

    solve: MajorityElement

---
+++ src/main/java/org/example/daily/MajorityElement_20260411_01.java
@@ -0,0 +1,11 @@
+package org.example.daily;
+public class MajorityElement_20260411_01 {
+    public int majorityElement(int[] nums) {
+        int cand = 0, cnt = 0;
+        for (int n : nums) {
+            if (cnt == 0) cand = n;
+            cnt += (n == cand) ? 1 : -1;
+        }
+        return cand;
+    }
+}
