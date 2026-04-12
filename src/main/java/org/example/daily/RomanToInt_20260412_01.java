0f5186793c77c565ec7e373be4b00edb5810a458
zhangzibo <zzb1230920@163.com>
2026-04-12 09:30:00 +0800

    solve: RomanToInt

---
+++ src/main/java/org/example/daily/RomanToInt_20260412_01.java
@@ -0,0 +1,16 @@
+package org.example.daily;
+public class RomanToInt_20260412_01 {
+    public int romanToInt(String s) {
+        int sum = 0, prev = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int cur = switch (s.charAt(i)) {
+                case 'I' -> 1; case 'V' -> 5; case 'X' -> 10;
+                case 'L' -> 50; case 'C' -> 100; case 'D' -> 500;
+                case 'M' -> 1000; default -> 0;
+            };
+            sum += (cur < prev) ? -cur : cur;
+            prev = cur;
+        }
+        return sum;
+    }
+}
