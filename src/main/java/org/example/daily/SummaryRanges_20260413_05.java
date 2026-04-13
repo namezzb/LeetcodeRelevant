0a12b48a04f3f8ee5d29aa1b165cc3ec49b3370e
zhangzibo <zzb1230920@163.com>
2026-04-13 17:30:00 +0800

    solve: SummaryRanges

---
+++ src/main/java/org/example/daily/SummaryRanges_20260413_05.java
@@ -0,0 +1,14 @@
+package org.example.daily;
+import java.util.List;
+import java.util.ArrayList;
+public class SummaryRanges_20260413_05 {
+    public List<String> summaryRanges(int[] nums) {
+        var r = new ArrayList<String>();
+        for (int i = 0; i < nums.length; i++) {
+            int s = nums[i];
+            while (i + 1 < nums.length && nums[i + 1] == nums[i] + 1) i++;
+            r.add(s == nums[i] ? String.valueOf(s) : s + "->" + nums[i]);
+        }
+        return r;
+    }
+}
