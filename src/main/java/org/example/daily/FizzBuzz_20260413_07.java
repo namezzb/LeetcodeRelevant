afef67bad53839f0d536414d3f3735b618219fc8
zhangzibo <zzb1230920@163.com>
2026-04-13 21:30:00 +0800

    solve: FizzBuzz

---
+++ src/main/java/org/example/daily/FizzBuzz_20260413_07.java
@@ -0,0 +1,15 @@
+package org.example.daily;
+import java.util.List;
+import java.util.ArrayList;
+public class FizzBuzz_20260413_07 {
+    public List<String> fizzBuzz(int n) {
+        var ans = new ArrayList<String>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 15 == 0) ans.add("FizzBuzz");
+            else if (i % 3 == 0) ans.add("Fizz");
+            else if (i % 5 == 0) ans.add("Buzz");
+            else ans.add(String.valueOf(i));
+        }
+        return ans;
+    }
+}
