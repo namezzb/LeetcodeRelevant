4374139b396b0b3b2c5d0c27e48cc05b79017bbf
zhangzibo <zzb1230920@163.com>
2026-04-16 13:30:00 +0800

    solve: RemoveDuplicates

---
+++ src/main/java/org/example/daily/RemoveDuplicates_20260416_03.java
@@ -0,0 +1,10 @@
+package org.example.daily;
+public class RemoveDuplicates_20260416_03 {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+        int k = 0;
+        for (int i = 1; i < nums.length; i++)
+            if (nums[i] != nums[k]) nums[++k] = nums[i];
+        return k + 1;
+    }
+}
