773389a0775fb11370429272cfa58dbff58be9e4
zhangzibo <zzb1230920@163.com>
2026-04-18 09:30:00 +0800

    solve: WordPattern

---
+++ src/main/java/org/example/daily/WordPattern_20260418_01.java
@@ -0,0 +1,16 @@
+package org.example.daily;
+import java.util.Map;
+import java.util.HashMap;
+public class WordPattern_20260418_01 {
+    public boolean wordPattern(String p, String s) {
+        String[] w = s.split(" ");
+        if (p.length() != w.length) return false;
+        var m = new HashMap<Character, String>();
+        for (int i = 0; i < p.length(); i++) {
+            char c = p.charAt(i);
+            if (m.containsKey(c)) { if (!m.get(c).equals(w[i])) return false; }
+            else { if (m.containsValue(w[i])) return false; m.put(c, w[i]); }
+        }
+        return true;
+    }
+}
