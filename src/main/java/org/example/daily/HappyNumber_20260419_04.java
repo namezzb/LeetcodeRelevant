7e0ed1d17ed3d4fc20c6c1a9521142afc8128879
zhangzibo <zzb1230920@163.com>
2026-04-19 15:00:00 +0800

    solve: HappyNumber

---
+++ src/main/java/org/example/daily/HappyNumber_20260419_04.java
@@ -0,0 +1,12 @@
+package org.example.daily;
+public class HappyNumber_20260419_04 {
+    public boolean isHappy(int n) {
+        int slow = n, fast = n;
+        do { slow = dsum(slow); fast = dsum(dsum(fast)); } while (slow != fast);
+        return slow == 1;
+    }
+    private int dsum(int n) {
+        int s = 0; while (n > 0) { int d = n % 10; s += d * d; n /= 10; }
+        return s;
+    }
+}
