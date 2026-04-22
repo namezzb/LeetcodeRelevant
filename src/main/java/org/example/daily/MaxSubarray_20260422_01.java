dd9813f5ffdf3ed0189534b31be055ece7ed85c4
zhangzibo <zzb1230920@163.com>
2026-04-22 09:30:00 +0800

    solve: MaxSubarray

---
+++ src/main/java/org/example/daily/MaxSubarray_20260422_01.java
@@ -0,0 +1,11 @@
+package org.example.daily;
+public class MaxSubarray_20260422_01 {
+    public int maxSubArray(int[] nums) {
+        int max = nums[0], cur = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            cur = Math.max(nums[i], cur + nums[i]);
+            max = Math.max(max, cur);
+        }
+        return max;
+    }
+}
