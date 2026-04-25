47e717bc15463d6658bbee13ef9d924ecdc1e01f
zhangzibo <zzb1230920@163.com>
2026-04-25 21:30:00 +0800

    solve: ClimbingStairs

---
+++ src/main/java/org/example/daily/ClimbingStairs_20260425_07.java
@@ -0,0 +1,9 @@
+package org.example.daily;
+public class ClimbingStairs_20260425_07 {
+    public int climbStairs(int n) {
+        if (n <= 2) return n;
+        int a = 1, b = 2;
+        for (int i = 3; i <= n; i++) { int t = a + b; a = b; b = t; }
+        return b;
+    }
+}
