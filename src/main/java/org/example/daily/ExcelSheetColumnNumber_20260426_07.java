14c1faa855a0faae8c42d07d3ed5dc5e28b53595
zhangzibo <zzb1230920@163.com>
2026-04-26 21:30:00 +0800

    solve: ExcelSheetColumnNumber

---
+++ src/main/java/org/example/daily/ExcelSheetColumnNumber_20260426_07.java
@@ -0,0 +1,8 @@
+package org.example.daily;
+public class ExcelSheetColumnNumber_20260426_07 {
+    public int titleToNumber(String s) {
+        int r = 0;
+        for (char c : s.toCharArray()) r = r * 26 + (c - 'A' + 1);
+        return r;
+    }
+}
