bf90c78254349444a8f4f4d82c914dd503c32f56
zhangzibo <zzb1230920@163.com>
2026-04-26 22:30:00 +0800

    solve: ReverseLinkedList

---
+++ src/main/java/org/example/daily/Extra_ReverseLinkedList_20260426_02.java
@@ -0,0 +1,11 @@
+package org.example.daily;
+public class Extra_ReverseLinkedList_20260426_02 {
+    static class ListNode { int val; ListNode next; ListNode(int v) { val = v; } }
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null, curr = head;
+        while (curr != null) {
+            ListNode next = curr.next; curr.next = prev; prev = curr; curr = next;
+        }
+        return prev;
+    }
+}
