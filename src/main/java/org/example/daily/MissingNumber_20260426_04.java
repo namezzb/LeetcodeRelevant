8d168799c7425f1024c6d4be5ff1d1c7f22800b3
zhangzibo <zzb1230920@163.com>
2026-04-26 15:00:00 +0800

    solve: MissingNumber

---
+++ src/main/java/org/example/daily/MissingNumber_20260426_04.java
@@ -0,0 +1,8 @@
+package org.example.daily;
+public class MissingNumber_20260426_04 {
+    public int missingNumber(int[] nums) {
+        int n = nums.length, sum = n * (n + 1) / 2;
+        for (int v : nums) sum -= v;
+        return sum;
+    }
+}
