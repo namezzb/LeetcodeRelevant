c0c40092b250c13c92dfeeaeb89af122b3fde61e
zhangzibo <zzb1230920@163.com>
2026-04-27 19:00:00 +0800

    solve: BinarySearch

---
+++ src/main/java/org/example/daily/BinarySearch_20260427_06.java
@@ -0,0 +1,12 @@
+package org.example.daily;
+public class BinarySearch_20260427_06 {
+    public int search(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (nums[m] == target) return m;
+            if (nums[m] < target) l = m + 1; else r = m - 1;
+        }
+        return -1;
+    }
+}
