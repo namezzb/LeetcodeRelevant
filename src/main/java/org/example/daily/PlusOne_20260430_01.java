893ee49cbbf5b3d21359b912800a6ba8dce0ad09
zhangzibo <zzb1230920@163.com>
2026-04-30 09:30:00 +0800

    solve: PlusOne

---
+++ src/main/java/org/example/daily/PlusOne_20260430_01.java
@@ -0,0 +1,11 @@
+package org.example.daily;
+public class PlusOne_20260430_01 {
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) { digits[i]++; return digits; }
+            digits[i] = 0;
+        }
+        int[] res = new int[digits.length + 1]; res[0] = 1;
+        return res;
+    }
+}
