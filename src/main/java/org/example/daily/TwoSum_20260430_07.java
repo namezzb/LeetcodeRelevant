23625f078a94f8d09be518dd4b0d6f4c8e49de96
zhangzibo <zzb1230920@163.com>
2026-04-30 21:30:00 +0800

    solve: TwoSum

---
+++ src/main/java/org/example/daily/TwoSum_20260430_07.java
@@ -0,0 +1,12 @@
+package org.example.daily;
+public class TwoSum_20260430_07 {
+    public int[] twoSum(int[] nums, int target) {
+        var map = new java.util.HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            int c = target - nums[i];
+            if (map.containsKey(c)) return new int[]{map.get(c), i};
+            map.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+}
